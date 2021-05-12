dc4f5c7cd51906d8292e11952efae120d7927eb1
igm4 <igm4@tpu.ru>
2021-05-13 04:23:48 +0700

    script edited

--- scripts/avgMarks.sql
+++ scripts/avgMarks.sql
@@ -1,6 +1,14 @@
-select distinct disciplines.name,
+# 1
+select distinct marks.disciplines_id,
                 round(avg(mark), 3) as 'Средняя оценка'
 from marks
 join disciplines on marks.disciplines_id = disciplines.disciplines_id
 where mark REGEXP '^-?[0-9]+$'
-group by name;
+group by marks.disciplines_id;
+# 2
+select distinct marks.disciplines_id,
+                round(avg(CONVERT(SUBSTRING_INDEX(mark,'-',-1),UNSIGNED INTEGER)),3)
+from marks
+join disciplines on marks.disciplines_id = disciplines.disciplines_id
+where mark REGEXP '^-?[0-9]+$'
+group by marks.disciplines_id;
