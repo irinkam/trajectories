e997f404a03231f16003f9d230af00bfbb4cac68
igm4 <igm4@tpu.ru>
2021-05-13 04:26:04 +0700

    script edited

--- scripts/avgMarks.sql
+++ scripts/avgMarks.sql
@@ -12,3 +12,15 @@ from marks
 join disciplines on marks.disciplines_id = disciplines.disciplines_id
 where mark REGEXP '^-?[0-9]+$'
 group by marks.disciplines_id;
+
+INSERT INTO disciplines(avgMark)
+SELECT temp.avgMark
+FROM (
+        select distinct marks.disciplines_id,
+                        round(avg(CONVERT(SUBSTRING_INDEX(mark,'-',-1),UNSIGNED INTEGER)),3) as 'avgMark'
+        from marks
+        join disciplines on marks.disciplines_id = disciplines.disciplines_id
+        where mark REGEXP '^-?[0-9]+$'
+        group by marks.disciplines_id)
+    AS temp
+WHERE disciplines_id = temp.disciplines_id
